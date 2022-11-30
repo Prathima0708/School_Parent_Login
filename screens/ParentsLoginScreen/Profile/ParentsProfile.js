bafd2a635c2b6ea5a445c4d0bf1a1ec8c4ce1023
zeeyan23 <zeeyanraza444@gmail.com>
2022-11-30 16:34:09 +0530

    updatedcode

--- screens/ParentsLoginScreen/Profile/ParentsProfile.js
+++ screens/ParentsLoginScreen/Profile/ParentsProfile.js
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Image, Dimensions, useWindowDimensions, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
 import {
   className,
   Section,
@@ -7,10 +7,30 @@ import {
   StudentPhoto,
   StudentRegNo,
 } from "../../../components/StudentItem/StudentItem";
-import { Box, Center, Pressable } from "native-base";
+import { Button as NativeButton, Divider,Avatar as NativeAvatar, } from "native-base";
+import ParentsHome from "../BottomTab/ParentsHome";
 
 const ParentsProfile = () => {
+
+  const [isActive,setIsActive]=useState(true);
+
+  const [parentInfoClicked,setParentInfoClicked]=useState(false);
+  const [studentInfoClicked,setStudentInfoClicked]=useState(true);
+
+  function studentInfoHanlder(){
+    setStudentInfoClicked(true);
+    setParentInfoClicked(false);
+    setIsActive(true);
+  }
+
+  function parentInfoHanlder(){
+    setParentInfoClicked(true);
+    setStudentInfoClicked(false);
+    setIsActive(true);
+  }
+
   return (
+    <>
     <View
       style={[
         styles.container,
@@ -20,7 +40,7 @@ const ParentsProfile = () => {
         },
       ]}
     >
-      <View style={{ flex: 3 }}>
+      <View style={{ flex: 0.6 }}>
         <View
           style={[
             styles.container,
@@ -62,16 +82,247 @@ const ParentsProfile = () => {
           </View>
         </View>
       </View>
-      <View style={{ flex: 9, backgroundColor: "darkorange" }}></View>
+        <View style={[styles.tabHeader]}>
+          <NativeButton 
+            onPress={studentInfoHanlder} 
+            variant="outline" 
+            colorScheme='darkBlue'
+            style={[
+              styles.buttonStyle,
+              studentInfoClicked && isActive && {borderBottomColor:'#4C49AB',borderBottomWidth:3,backgroundColor:'#D6D7FF'}]}>Student Info</NativeButton>
+          <Divider
+            bg="#4C49AB"
+            thickness="2"
+            orientation="vertical"
+            // style={{ left: "60%" }}
+          />
+          <NativeButton 
+            onPress={parentInfoHanlder} 
+            variant="outline" 
+            colorScheme='darkBlue'
+            style={[
+              styles.buttonStyle,
+              parentInfoClicked && isActive && {borderBottomColor:'#4C49AB',borderBottomWidth:3,backgroundColor:'#D6D7FF'}]}>Parent Info</NativeButton>
+        </View>
+
+      {studentInfoClicked && 
+        <View
+          style={[styles.flexStyle]}>
+          <View style={{ flex: 1}}>
+            <ScrollView>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Date Of Birth</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>12/10/1999</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Gendar</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>male</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Admission Date</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>01/02/2006</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Student Address</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>123 block,street ABC,NH-66</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>City</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>Udupi</Text>
+                </View>
+              </View>
+
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>State</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>Karnataka</Text>
+                </View>
+              </View>
+
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Country</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>India</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Pincode</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>5722887</Text>
+                </View>
+              </View>
+              <View style={[styles.viewStyle]}>
+                <View style={{ flex: 1}} >
+                  <Text style={styles.labelStyle}>Academic Year</Text>
+                </View>
+                <View style={{ flex: 1 }} >
+                  <Text style={styles.labelStyle}>2006</Text>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      }
+      {parentInfoClicked && 
+        <View
+          style={styles.flexStyle}>
+          <View style={{ flex: 8, top:'1%' }}>
+            <ScrollView>
+              <View style={[{flex:1}, {
+                flexDirection: "column",
+                top:'2%'
+              }]}>
+                <View style={{ flex: 1 }} >
+                  <View style={[{flex:1}, {flexDirection: "row"}]}>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {
+                        flexDirection: "column"
+                      }]}>
+                        <View style={{ flex: 1 }} >
+                          <NativeAvatar 
+                            bg="purple.600" 
+                            alignSelf="center" 
+                            size="xl"
+                            source={{uri: "https://www.w3schools.com/css/img_lights.jpg"}}>
+                          </NativeAvatar>
+                        </View>
+                        <View style={{ flex: 1,alignItems:'center' }} >
+                          <Text style={[styles.labelStyle]}>Father</Text>
+                        </View>
+                      </View>
+                    </View>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {flexDirection: "column"}]}>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>ABCD</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>9018212345</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>Lawyer</Text>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View style={styles.space} />
+                <View style={{ flex: 1,}} >
+                 <View style={[{flex:1}, {flexDirection: "row"}]}>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {
+                          flexDirection: "column"
+                        }]}>
+                          <View style={{ flex: 1 }} >
+                            <NativeAvatar 
+                              bg="purple.600" 
+                              alignSelf="center" 
+                              size="xl"
+                              source={{uri: "https://www.w3schools.com/css/img_lights.jpg"}}>
+                            </NativeAvatar>
+                          </View>
+                          <View style={{ flex: 1,alignItems:'center' }} >
+                            <Text style={[styles.labelStyle]}>Mother</Text>
+                          </View>
+                        </View>
+                    </View>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {flexDirection: "column"}]}>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>PQR</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>0978231323</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>Teacher</Text>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View style={styles.space} />
+                <View style={{ flex: 1 }} >
+                  <View style={[{flex:1}, {flexDirection: "row"}]}>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {
+                          flexDirection: "column"
+                        }]}>
+                          <View style={{ flex: 1 }} >
+                            <NativeAvatar 
+                              bg="purple.600" 
+                              alignSelf="center" 
+                              size="xl"
+                              source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}}>
+                            </NativeAvatar>
+                          </View>
+                          <View style={{ flex: 1,alignItems:'center' }} >
+                            <Text style={[styles.labelStyle]}>Guardian</Text>
+                          </View>
+                        </View>
+                    </View>
+                    <View style={{ flex: 1 }} >
+                      <View style={[{flex:1}, {flexDirection: "column"}]}>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>IOS</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>5676890912</Text>
+                        </View>
+                        <View style={{ flex: 1 }} >
+                          <Text style={styles.labelStyle}>Businessman</Text>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      }
     </View>
+    <View style={{ flex: 1 }}>
+      <ParentsHome />
+    </View>
+    </>
   );
 };
 
 export default ParentsProfile;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     padding: 20,
+  },
+  space: {
+    width: 20,
+    height: 10,
   },
   imageContainer: {
     backgroundColor: "white",
@@ -84,4 +335,37 @@ const styles = StyleSheet.create({
     height: 90,
     width: 110,
   },
+  fontStyle:{
+    fontFamily:'HindBold',
+    fontSize:16
+  },
+  tabHeader:{
+    flexDirection:'row',
+    // top:'20%',
+    justifyContent:'space-between',
+  },
+  buttonStyle:{
+    width:'50%',
+    borderTopWidth:0,
+    borderLeftWidth:0,
+    borderRightWidth:0,
+    borderRadius:0
+  },
+  // space: {
+  //   width: 20,
+  //   height: 20,
+  // },
+  labelStyle:{
+    fontFamily:'HindBold'
+  },
+  flexStyle:{
+    flex: 1,
+    flexDirection: "column", 
+    backgroundColor: "white"
+  },
+  viewStyle:{
+    flex:1,
+    flexDirection: "row",
+    paddingVertical:10
+  }
 });
