bbf30ec35073f9b62440a2cd2c7c21ff316df416
Prathima0708 <kotianpriya64@gmail.com>
2023-01-31 16:49:23 +0530

    updated code

--- screens/TeachersLoginScreen/Calendar/TeachersCalendar.js
+++ screens/TeachersLoginScreen/Calendar/TeachersCalendar.js
@@ -122,6 +122,7 @@ const TeachersCalendar = () => {
   const [isToDateFocused, setIsToDateFocused] = useState(false);
   const [calendarViewBtnPressed, setCalendarViewBtnPressed] = useState(false);
   const [btn, setBtn] = useState(false);
+  const [receivedNotification, setReceivedNotification] = useState([]);
 
   const [forCalendarList, setForCalendarList] = useState({
     backgroundColor: "#1E84A4",
@@ -243,6 +244,7 @@ const TeachersCalendar = () => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       async (notification) => {
         console.log("Notification received");
+
         //  console.log("id is", specificData.id);
 
         try {
@@ -252,6 +254,7 @@ const TeachersCalendar = () => {
               isNotified: true,
             }
           );
+          // setSpecificData(response.data);
           //  console.log(response.data);
         } catch (error) {
           console.error(error);
@@ -970,6 +973,7 @@ const TeachersCalendar = () => {
   }
 
   function cardPressedHandler(filteredData) {
+    console.log(filteredData.titlee);
     setSpecificData(filteredData);
 
     setOpenCardModal(true);
@@ -1549,7 +1553,8 @@ const TeachersCalendar = () => {
                               // Try setting `flexDirection` to `"row"`.
                               flex: 1,
                               flexDirection: "column",
-                              borderBottomWidth: filteredlist.length>1 ? 1 : 0,
+                              borderBottomWidth:
+                                filteredlist.length > 1 ? 1 : 0,
                               borderBottomColor: "grey",
                             },
                           ]}
@@ -1561,12 +1566,14 @@ const TeachersCalendar = () => {
                                   // Try setting `flexDirection` to `"row"`.
                                   flex: 1,
                                   flexDirection: "row",
-                                  marginVertical:10
+                                  marginVertical: 10,
                                 },
                               ]}
                             >
-                              <View style={{ flex: 0.35 }}>
-                                <Text style={styles.cardTextStyle}>Title :</Text>
+                              <View style={{ flex: 0.35, left: 0 }}>
+                                <Text style={styles.cardTextStyle}>
+                                  Title :
+                                </Text>
                               </View>
                               <View style={{ flex: 1 }}>
                                 <Text style={styles.textStyle}>
@@ -1601,7 +1608,9 @@ const TeachersCalendar = () => {
                                     </Text>
                                   </View>
                                   <View style={{ flex: 1.1 }}>
-                                    <Text style={[styles.textStyle,{left:5}]}>
+                                    <Text
+                                      style={[styles.textStyle, { left: 5 }]}
+                                    >
                                       {moment(data.startdate).format(
                                         "DD/MM/YYYY"
                                       )}
@@ -1620,7 +1629,9 @@ const TeachersCalendar = () => {
                                   ]}
                                 >
                                   <View style={{ flex: 0.8 }}>
-                                    <Text style={styles.cardTextStyle}>To :</Text>
+                                    <Text style={styles.cardTextStyle}>
+                                      To :
+                                    </Text>
                                   </View>
                                   <View style={{ flex: 1 }}>
                                     <Text style={styles.textStyle}>
@@ -1633,7 +1644,7 @@ const TeachersCalendar = () => {
                               </View>
                             </View>
                           </View>
-                          <View style={{ flex: 1,marginVertical:10 }}>
+                          <View style={{ flex: 1, marginVertical: 10 }}>
                             <View
                               style={[
                                 {
